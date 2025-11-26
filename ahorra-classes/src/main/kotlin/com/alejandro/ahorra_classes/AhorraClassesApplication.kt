f979cc24c48d515434cdcc5fc1976a439a070529
aleBello1 <mgranados@ahorra.io>
2025-11-26 10:33:38 -0600

    git flow

---
+++ ahorra-classes/src/main/kotlin/com/alejandro/ahorra_classes/AhorraClassesApplication.kt
@@ -0,0 +1,11 @@
+package com.alejandro.ahorra_classes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AhorraClassesApplication
+
+fun main(args: Array<String>) {
+	runApplication<AhorraClassesApplication>(*args)
+}
---
+++ ahorra-classes/src/test/kotlin/com/alejandro/ahorra_classes/AhorraClassesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alejandro.ahorra_classes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AhorraClassesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
